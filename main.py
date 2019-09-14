0a6ac3f6ccfbc523ac42d4402db8168733e2dd41
Alex Zhao <oatmonster@gmail.com>
2019-09-14 13:58:11 -0700

    Added some data visualization

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+import os
+import random
 
-x1 = tf.constant([1,2,3,4])
-x2 = tf.constant([5,6,7,8])
 
-result = tf.multiply(x1,x2)
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
 
-sess = tf.Session()
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) for f in os.listdir(label_directory) if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
 
-print(sess.run(result))
+train_data_directory = "./Training/"
+test_data_directory = "./Testing/"
 
-sess.close()
+images, labels = load_data(train_data_directory)
 
+# signs = [random.choice(images) for i in range(4)]
+
+# for i in range(len(signs)):
+#     plt.subplot(1, 4, i + 1)
+#     plt.axis('off')
+#     plt.imshow(signs[i])
+#     plt.subplots_adjust(wspace = 0.5)
+# plt.show()
+
+unique_lables = set(labels)
+
+plt.figure(figsize = (15, 15))
+
+i = 1
+
+for label in unique_lables:
+    image = images[labels.index(label)]
+    plt.subplot(8, 8, i)
+    plt.axis('off')
+    plt.title("label {0} ({1})".format(label, labels.count(label)))
+    i += 1
+    plt.imshow(image)
+plt.show()
